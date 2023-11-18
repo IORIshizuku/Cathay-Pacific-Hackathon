8da132a04bfbe2d40895cc9035e7cc0d3f38f959
IORIshizuku <whitewind1995@gmail.com>
2023-11-18 19:02:26 +0800

    added update new comment feature

---
+++ src/components/comment.js
@@ -0,0 +1,25 @@
+function submitComment(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get the user's comment from the input field
+    var commentInput = document.getElementById("commentInput");
+    var commentText = commentInput.value;
+
+    // Create a new list item for the comment
+    var newComment = document.createElement("li");
+    newComment.className = "comment";
+    newComment.innerHTML = `
+        <div class="comment-user">
+            <img src="../src/components/images/user_icon.jpeg" alt="User Icon" class="user-icon me-2" style="width: 30px; height: 30px; border-radius: 50%;">
+            <strong>Franklin Poon</strong>
+        </div>
+        <p class="comment-text">${commentText}</p>
+    `;
+
+    // Add the new comment to the comments list
+    var commentList = document.getElementById("commentList");
+    commentList.appendChild(newComment);
+
+    // Clear the comment input field
+    commentInput.value = "";
+}
